5ebdd894c2a7cc578c879a6bfa2b73d2f58c2ee4
Ignacio <ignaciomendez6@gmail.com>
2025-02-06 10:36:57 -0300

    Initial Commit

---
+++ MobileChallengeUala/MobileChallengeUalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileChallengeUalaApp.swift
+//  MobileChallengeUala
+//
+//  Created by Nacho Mendez on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MobileChallengeUalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
